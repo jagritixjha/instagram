af5995e2f5a7322ea3be9a3284e46a15e2bebb0e
jagriti <im.jagriti.jha@gmail.com>
2024-08-06 23:18:39 +0530

    added padding constant variable

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const double webConstraint = 600;
 Color primaryColor = Colors.blueAccent.shade700;
+EdgeInsetsGeometry padding =
+    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 40);
