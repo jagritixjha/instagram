7d2ec633eb65fbedc67798526722863c4abbe900
jagriti <im.jagriti.jha@gmail.com>
2024-08-11 22:15:46 +0530

    added profile screen ui

--- lib/view/profile_screen/profile_screen.dart
+++ lib/view/profile_screen/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:instagram/utils/constants.dart';
+import 'package:instagram/widgets/action_button.dart';
 import 'package:instagram/widgets/small_text.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -9,20 +12,139 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const SmallText(
+          text: 'jagritixjha',
+        ),
         actions: [
           IconButton(
             onPressed: () {
               FirebaseAuth.instance.signOut();
             },
-            icon: Icon(Icons.logout_rounded),
+            icon: const Icon(Icons.logout_rounded),
           )
         ],
       ),
-      body: Center(
-        child: SmallText(
-          text: 'Profile Screen',
-          fontSize: 20,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      'https://i.pinimg.com/564x/77/9c/ae/779caee3e30b3e6c88acd36dc62a5426.jpg'),
+                ),
+                const Spacer(),
+                CustomRichText(
+                  str: '0',
+                  subStr: 'posts',
+                ),
+                const Spacer(),
+                CustomRichText(
+                  str: '104',
+                  subStr: 'followers',
+                ),
+                const Spacer(),
+                CustomRichText(
+                  str: '119',
+                  subStr: 'following',
+                ),
+              ],
+            ),
+          ),
+          BuildSection2(),
+          SizedBox(
+            height: 14,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomRichText extends StatelessWidget {
+  CustomRichText({
+    super.key,
+    required this.str,
+    required this.subStr,
+  });
+  String str;
+  String subStr;
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        text: str,
+        style: GoogleFonts.poppins(
+          height: 1.2,
+          textStyle: const TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+          ),
         ),
+        children: [
+          TextSpan(
+            text: '\n$subStr',
+            style: GoogleFonts.poppins(
+              textStyle:
+                  const TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BuildSection2 extends StatelessWidget {
+  const BuildSection2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 8,
+          ),
+          const SmallText(
+            text: 'Jagriti Jha',
+            textAlign: TextAlign.start,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          const SmallText(
+            text: 'Chasing life\'s destiny',
+            textAlign: TextAlign.start,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SecondaryActionButton(
+                text: 'Follow',
+                isProfile: true,
+                bgColor: primaryColor,
+              ),
+              SecondaryActionButton(
+                text: 'Message',
+                isProfile: true,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
---
+++ lib/widgets/action_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/constants.dart';
+import 'package:instagram/widgets/small_text.dart';
+
+class SecondaryActionButton extends StatelessWidget {
+  SecondaryActionButton({
+    super.key,
+    required this.text,
+    this.onPressed,
+    this.isProfile = false,
+    this.bgColor,
+  });
+
+  String text;
+  bool isProfile;
+  Color? bgColor;
+
+  void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        alignment: Alignment.center,
+        backgroundColor: bgColor ?? Colors.indigo.shade50,
+        visualDensity: VisualDensity.compact,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        minimumSize: isProfile ? const Size(180, 40) : null,
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+      ),
+      child: SmallText(
+        text: text,
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: bgColor != null ? Colors.white : Colors.black,
+      ),
+    );
+  }
+}
